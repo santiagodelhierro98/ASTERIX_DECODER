269bd831c5a8500f89b106932a24bc884b89eec5
Shuan Hernandez <email@example.com>
2020-03-09 18:42:31 +0100

    CAT10 version pre-definitiva

--- CLASSES/CAT10.cs
+++ CLASSES/CAT10.cs
@@ -14,9 +14,9 @@ namespace CLASSES
         string Message_Type;
         string Data_Source_ID;
         string Target_Rep_Descript;
-        double[] Pos_PolarCoord; //rho,theta
-        double[] Pos_WGS84;
-        double[] Pos_Cartesian;
+        double[] Pos_PolarCoord = new double[2]; //rho,theta
+        double[] Pos_WGS84 = new double[2];
+        int [] Pos_Cartesian = new int[2];
         string Mode3A_Code;
         string FL_Binary;
         double Height;   //ft
@@ -24,17 +24,17 @@ namespace CLASSES
         double Time_Day; //seconds
         int Track_Num;
         string Track_Status;
-        double[] Track_Vel_Polar;    // (NM/s, degrees)
-        double[] Track_Vel_Cartesian;    // m/s
-        double Acceleration; // m/s^2
+        double[] Track_Vel_Polar = new double[2];    // (NM/s, degrees)
+        double[] Track_Vel_Cartesian = new double[2];    // m/s
+        double[] Acceleration = new double[2]; // m/s^2
         int Target_Add;
         string Target_ID;
         string Mode_SMB;
-        double[] Target_Size_Heading;    // m,degrees
-        double[] Presence;   // rho,theta
+        double[] Target_Size_Heading = new double[2];    // m,degrees
+        double[] Presence = new double[2];   // rho,theta
         string Fleet_ID;
         string Pre_Prog_Message;
-        double StndrdDev_Position; // m^2
+        double[] StndrdDev_Position = new double[3]; // m^2
         string Sys_Status;
 
         public void Decode10(string[] paquete)
@@ -176,36 +176,92 @@ namespace CLASSES
                 string ToD2 = M.Octeto_A_Bin(paquete0[contador + 1]);
                 string ToD3 = M.Octeto_A_Bin(paquete0[contador + 2]);
 
-                string ToD_Invertido = ToD1 + ToD2 + ToD3;
-                char[] ToD_bin = ToD_Invertido.ToCharArray();
-                Array.Reverse(ToD_bin);
+                string ToD = ToD1 + ToD2 + ToD3;                
 
-                for (int i = 0; i < ToD_bin.Length; i++)
-                {
-                    if (ToD_bin[i].ToString() == "1") { Time_Day = Time_Day + Math.Pow(2, -7 + i); }
-                    else { }
-                }
+                Time_Day = Convert.ToInt32(ToD, 2)/128.0;
                 contador += 3;
             }
             if (FSPEC[4] == "1")
             {
                 // Item I010/041: Position WGS-84
+                string position1 = M.Octeto_A_Bin(paquete0[contador]);
+                string position2 = M.Octeto_A_Bin(paquete0[contador + 1]);
+                string position3 = M.Octeto_A_Bin(paquete0[contador + 2]);
+                string position4 = M.Octeto_A_Bin(paquete0[contador + 3]);
+                string position5 = M.Octeto_A_Bin(paquete0[contador + 4]);
+                string position6 = M.Octeto_A_Bin(paquete0[contador + 5]);
+                string position7 = M.Octeto_A_Bin(paquete0[contador + 6]);
+                string position8 = M.Octeto_A_Bin(paquete0[contador + 7]);
+
+                string lat_bin = position1 + position2 + position3 + position4;
+                string lon_bin = position5 + position6 + position7 + position8;
+
+                Pos_WGS84[0] = M.ComplementoA2(lat_bin) * (180 / Math.Pow(2, 31));
+                Pos_WGS84[1] = M.ComplementoA2(lon_bin) * (180 / Math.Pow(2, 31));
+
+                contador += 8;
             }
             if (FSPEC[5] == "1")
             {
                 // Item I010/040: Position Polar
+                string PolarR1 = M.Octeto_A_Bin(paquete0[contador]);
+                string PolarR2 = M.Octeto_A_Bin(paquete0[contador + 1]);
+                string PolarT1 = M.Octeto_A_Bin(paquete0[contador + 2]);
+                string PolarT2 = M.Octeto_A_Bin(paquete0[contador + 3]);
+
+                string PolarR = PolarR1 + PolarR2;
+                string PolarT = PolarT1 + PolarT2;
+
+                Pos_PolarCoord[0] = Convert.ToInt32(PolarR, 2);
+                Pos_PolarCoord[1] = Convert.ToInt32(PolarT, 2) * (360.0 / Math.Pow(2, 16));
+
+                contador += 4;
             }
             if (FSPEC[6] == "1")
             {
                 // Item I010/042: Position Cartesian
+                string Cartx1 = M.Octeto_A_Bin(paquete0[contador]);
+                string Cartx2 = M.Octeto_A_Bin(paquete0[contador + 1]);
+                string Carty1 = M.Octeto_A_Bin(paquete0[contador + 2]);
+                string Carty2 = M.Octeto_A_Bin(paquete0[contador + 3]);
+
+                string CartX = Cartx1 + Cartx2;
+                string CartY = Carty1 + Carty2;
+
+                Pos_Cartesian[0] = Convert.ToInt32(M.ComplementoA2(CartX));
+                Pos_Cartesian[1] = Convert.ToInt32(M.ComplementoA2(CartY));
+
+                contador += 4;
             }
             if (FSPEC[7] == "1")
             {
                 // Item I010/200: Track Velocity Polar
+                string polar_vel1 = M.Octeto_A_Bin(paquete0[contador]);
+                string polar_vel2 = M.Octeto_A_Bin(paquete0[contador + 1]);
+                string polar_vel3 = M.Octeto_A_Bin(paquete0[contador + 2]);
+                string polar_vel4 = M.Octeto_A_Bin(paquete0[contador + 3]);
+
+                string track_gs = polar_vel1 + polar_vel2;
+                string track_ta = polar_vel3 + polar_vel4;
+
+                Track_Vel_Cartesian[0] = Convert.ToInt32(track_gs, 2) * Math.Pow(2, -14);
+                Track_Vel_Cartesian[1] = Convert.ToInt32(track_ta, 2) * (360.0/Math.Pow(2, 16));
+                contador += 4;
             }
             if (FSPEC[8] == "1")
             {
                 // Item I010/202: Track Velocity Cartesian
+                string track_vel1 = M.Octeto_A_Bin(paquete0[contador]);
+                string track_vel2 = M.Octeto_A_Bin(paquete0[contador + 1]);
+                string track_vel3 = M.Octeto_A_Bin(paquete0[contador + 2]);
+                string track_vel4 = M.Octeto_A_Bin(paquete0[contador + 3]);
+
+                string track_vx = track_vel1 + track_vel2;
+                string track_vy = track_vel3 + track_vel4;
+              
+                Track_Vel_Cartesian[0] = M.ComplementoA2(track_vx) * 0.25;
+                Track_Vel_Cartesian[1] = M.ComplementoA2(track_vy) * 0.25;
+                contador += 4;
             }
             if (FSPEC[9] == "1")
             {
@@ -354,62 +410,212 @@ namespace CLASSES
             if (FSPEC[14] == "1")
             {
                 // Item I010/250: Mode S MB Data
+                string REP = M.Octeto_A_Bin(paquete0[contador]);
+                string SMB2 = M.Octeto_A_Bin(paquete0[contador + 1]);
+                string SMB3 = M.Octeto_A_Bin(paquete0[contador + 2]);
+                string SMB4 = M.Octeto_A_Bin(paquete0[contador + 3]);
+                string SMB5 = M.Octeto_A_Bin(paquete0[contador + 4]);
+                string SMB6 = M.Octeto_A_Bin(paquete0[contador + 5]);
+                string SMB7 = M.Octeto_A_Bin(paquete0[contador + 6]);
+                string SMB8 = M.Octeto_A_Bin(paquete0[contador + 7]);
+                string BDS = M.Octeto_A_Bin(paquete0[contador + 8]);
+                char[] BDS_bin = BDS.ToCharArray();
+
+                Mode_SMB = "REP: " + REP;
+                Mode_SMB = Mode_SMB + "/MB: " + Convert.ToInt32(SMB2 + SMB3 + SMB4 + SMB5 + SMB6 + SMB7 + SMB8, 2);
+                Mode_SMB = Mode_SMB + "/BDS1: " + BDS_bin[0].ToString() + BDS_bin[1].ToString() + BDS_bin[2].ToString() + BDS_bin[3].ToString();
+                Mode_SMB = Mode_SMB + "/BDS2: " + BDS_bin[4].ToString() + BDS_bin[5].ToString() + BDS_bin[6].ToString() + BDS_bin[7].ToString();
+
+                contador += 9;
             }
             if (FSPEC[15] == "1")
             {
                 // Item I010/300: Vehicle Fleet ID
+                string fleetID = M.Octeto_A_Bin(paquete0[contador]);
+                int VFI = Convert.ToInt32(fleetID, 2);
+                if (VFI == 0) { Fleet_ID = "Unknown"; }
+                if (VFI == 1) { Fleet_ID = "ATC Equipment maintainance"; }
+                if (VFI == 2) { Fleet_ID = "Airport maintainance"; }
+                if (VFI == 3) { Fleet_ID = "Fire"; }
+                if (VFI == 4) { Fleet_ID = "Bird scarer"; }
+                if (VFI == 5) { Fleet_ID = "Snow plough"; }
+                if (VFI == 6) { Fleet_ID = "Runway sweeper"; }
+                if (VFI == 7) { Fleet_ID = "Emergency"; }
+                if (VFI == 8) { Fleet_ID = "Police"; }
+                if (VFI == 9) { Fleet_ID = "Bus"; }
+                if (VFI == 10) { Fleet_ID = "Tug (push/tow)"; }
+                if (VFI == 11) { Fleet_ID = "Grass cutter"; }
+                if (VFI == 12) { Fleet_ID = "Fuel"; }
+                if (VFI == 13) { Fleet_ID = "Baggage"; }
+                if (VFI == 14) { Fleet_ID = "Catering"; }
+                if (VFI == 15) { Fleet_ID = "Aircraft maintainance"; }
+                if (VFI == 16) { Fleet_ID = "Flyco (follow me)"; }
+
+                contador += 1;
             }
             if (FSPEC[16] == "1")
             {
                 // Item I010/090: Flight Level in Binary
+                string FL1 = M.Octeto_A_Bin(paquete0[contador]);
+                string FL2 = M.Octeto_A_Bin(paquete0[contador + 1]);
+
+                string FL = FL1 + FL2;
+                char[] FL_bin = FL.ToCharArray();
+
+                if (FL_bin[0].ToString() == "0") { FL_Binary = "V: Code Validated"; }
+                if (FL_bin[0].ToString() == "1") { FL_Binary = "V: Code Not Validated"; }
+
+                if (FL_bin[1].ToString() == "0") { FL_Binary = FL_Binary + "G: Default"; }
+                if (FL_bin[1].ToString() == "1") { FL_Binary = FL_Binary + "G: Garbled Code"; }
+
+                FL = new string(FL_bin, 2, 15);
+                FL_Binary = FL_Binary + "/FL: " + Convert.ToInt32(FL, 2);
+
+                contador += 2;
             }
             if (FSPEC[17] == "1")
             {
-                // Item I010/091: Measured High
+                // Item I010/091: Measured Height
+                string height1 = M.Octeto_A_Bin(paquete0[contador]);
+                string height2 = M.Octeto_A_Bin(paquete0[contador + 1]);
+
+                string height = height1 + height2;
+                Height = Convert.ToInt32(height) * 6.25;
+
+                contador += 2;
             }
             if (FSPEC[18] == "1")
             {
                 // Item I010/270: Target Size and Orientation
+                string length = M.Octeto_A_Bin(paquete0[contador]);
+                char[] length_v = length.ToCharArray();
+                string length_bin = new string(length_v, 0, 6);
+                Target_Size_Heading[0] = Convert.ToInt32(length_bin, 2);
+                
+                if (length_v[7].ToString() == "1")
+                {
+                    string length1 = M.Octeto_A_Bin(paquete0[contador + 1]);
+                    char[] length1_v = length1.ToCharArray();
+                    string lenght1_bin = new string(length1_v, 0, 6);
+                    Target_Size_Heading[1] = Convert.ToInt32(lenght1_bin, 2) * (360.0 / 128.0);
+
+                    if (length1_v[7].ToString() == "1")
+                    {
+                        string length2 = M.Octeto_A_Bin(paquete0[contador + 2]);
+                        char[] length2_v = length2.ToCharArray();
+                        string lenght2_bin = new string(length2_v, 0, 6);
+                        Target_Size_Heading[2] = Convert.ToInt32(lenght2_bin, 2);
+
+                        contador += 3;
+                    }
+                    if (length1_v[7].ToString() == "0") { contador += 2; }
+                }
+                if (length_v[7].ToString() == "0") { contador += 1; }
             }
             if (FSPEC[19] == "1")
             {
                 // Item I010/550: System Status
+                string stat = M.Octeto_A_Bin(paquete0[contador]);
+                char[] stat_v = stat.ToCharArray();
+
+                int NOGO = Convert.ToInt32(stat_v[0].ToString() + stat_v[1].ToString(), 2);
+                if (NOGO == 0) { Sys_Status = "NOGO: Operational"; }
+                if (NOGO == 1) { Sys_Status = "NOGO: Degraded"; }
+                if (NOGO == 2) { Sys_Status = "NOGO: NOGO"; }
+
+                if (stat_v[2].ToString() == "0") { Sys_Status = Sys_Status + "/OVL: No overload"; }
+                if (stat_v[2].ToString() == "1") { Sys_Status = Sys_Status + "/OVL: Overload"; }
+
+                if (stat_v[3].ToString() == "0") { Sys_Status = Sys_Status + "/TSV: Valid"; }
+                if (stat_v[3].ToString() == "1") { Sys_Status = Sys_Status + "/TSV: Invalid"; }
+
+                if (stat_v[4].ToString() == "0") { Sys_Status = Sys_Status + "/DIV: Normal Operation"; }
+                if (stat_v[4].ToString() == "1") { Sys_Status = Sys_Status + "/DIV: Diversity degraded"; }
+
+                if (stat_v[5].ToString() == "0") { Sys_Status = Sys_Status + "/TTF: Test Target Operative"; }
+                if (stat_v[5].ToString() == "1") { Sys_Status = Sys_Status + "/TTF: Test Target Failure"; }
+
+                contador += 1;
             }
             if (FSPEC[20] == "1")
             {
                 // Item I010/310: Pre-programmed Message
+                string messg = M.Octeto_A_Bin(paquete0[contador]);
+                char[] messg_v = messg.ToCharArray();
+
+                if (messg_v[0].ToString() == "0") { Pre_Prog_Message = "TRB: Default"; }
+                if (messg_v[0].ToString() == "1") { Pre_Prog_Message = "TRB: In Trouble"; }
+
+                string MSGbin = new string(messg_v, 1, 7);
+                int MSG = Convert.ToInt32(MSGbin, 2);
+
+                if (MSG == 1) { Pre_Prog_Message = Pre_Prog_Message + "/MSG: Towing Aircraft"; }
+                if (MSG == 2) { Pre_Prog_Message = Pre_Prog_Message + "/MSG: 'Follow Me' operation"; }
+                if (MSG == 3) { Pre_Prog_Message = Pre_Prog_Message + "/MSG: Runway Check"; }
+                if (MSG == 4) { Pre_Prog_Message = Pre_Prog_Message + "/MSG: Emergency Operation"; }
+                if (MSG == 5) { Pre_Prog_Message = Pre_Prog_Message + "/MSG: Work in progress"; }
+
+                contador += 1;
             }
             if (FSPEC[21] == "1")
             {
                 // Item I010/500: Standard Deviation of Position
+                string SDx = M.Octeto_A_Bin(paquete0[contador]);
+                string SDy = M.Octeto_A_Bin(paquete0[contador + 1]);
+                string SDxy1 = M.Octeto_A_Bin(paquete0[contador + 2]);
+                string SDxy2 = M.Octeto_A_Bin(paquete0[contador + 3]);
+
+                StndrdDev_Position[0] = Convert.ToInt32(SDx, 2) * 0.25;
+                StndrdDev_Position[1] = Convert.ToInt32(SDy, 2) * 0.25;
+
+                string SDxy = SDxy1 + SDxy2;
+                StndrdDev_Position[2] = Convert.ToInt32(SDxy, 2) * 0.25;
+
+                contador += 4;
             }
             if (FSPEC[22] == "1")
             {
                 // Item I010/280: Presence
+                string REP = M.Octeto_A_Bin(paquete0[contador]);
+                string DRHO = M.Octeto_A_Bin(paquete0[contador + 1]);
+                string DTHETA = M.Octeto_A_Bin(paquete0[contador + 2]);
+
+                Presence[0] = Convert.ToInt32(REP, 2);
+                Presence[1] = Convert.ToInt32(DRHO, 2);
+                Presence[2] = Convert.ToInt32(DTHETA, 2) * 0.15;
+
+                contador += 3;
             }
             if (FSPEC[23] == "1")
             {
                 // Item I010/131: Amplitude of Primary Plot
+                string amplitude = M.Octeto_A_Bin(paquete0[contador]);
+                Amplitude = Convert.ToInt32(amplitude, 2);
+
+                contador += 1;
             }
             if (FSPEC[24] == "1")
             {
                 // Item I010/210: Calculated Acceleration
+                string Ax = M.Octeto_A_Bin(paquete0[contador]);
+                string Ay = M.Octeto_A_Bin(paquete0[contador + 1]);
+
+                Acceleration[0] = M.ComplementoA2(Ax) * 0.25;
+                Acceleration[1] = M.ComplementoA2(Ay) * 0.25;
+
+                contador += 2;
             }
 
             // FSPEC[25] no tiene Data Items, es de "spare"
 
-            if (FSPEC[26] == "1")
-            {
-                // Item I010/040: Position Polar
-            }
-            if (FSPEC[27] == "1")
-            {
-                // SP: Special Purpose Field
-            }
-            if (FSPEC[28] == "1")
-            {
-                // RE: Reserved Expansion Field
-            }
+            //if (FSPEC[26] == "1")
+            //{
+            //    // SP: Special Purpose FieldS
+            //}
+            //if (FSPEC[27] == "1")
+            //{
+            //    // RE: Reserved Expansion Field
+            //}
         }
     }
 }
