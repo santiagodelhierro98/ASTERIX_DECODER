e5d69fe3aa92e861599265e2f8a70c2c8ac26a26
Shuan Hernandez <email@example.com>
2020-03-27 17:12:51 +0100

    .

--- ASTERIX_APP/Inicio.xaml.cs
+++ ASTERIX_APP/Inicio.xaml.cs
@@ -20,6 +20,8 @@ namespace ASTERIX_APP
         public Inicio()
         {
             InitializeComponent();
+            Window win = new Window();
+            win.WindowStartupLocation = WindowStartupLocation.CenterScreen;
         }
         private void Start_Click(object sender, RoutedEventArgs e)
         {
