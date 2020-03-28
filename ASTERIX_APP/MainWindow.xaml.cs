ac0a9de661485a01dc984d1e294301e8c8e5beef
Shuan Hernandez <email@example.com>
2020-03-28 22:17:22 +0100

    macaaaaaaaGUNDEU

--- ASTERIX_APP/MainWindow.xaml.cs
+++ ASTERIX_APP/MainWindow.xaml.cs
@@ -22,11 +22,11 @@ namespace ASTERIX_APP
 {
     public partial class MainWindow : Window
     {
-        List<CAT21> ListaCAT21;
         Fichero F;
-        public MainWindow(List<CAT21> ListaCAT21)
+        public MainWindow()
         {
             InitializeComponent();
+
             Instructions_Label.Visibility = Visibility.Visible; ;
             Instructions_Label.FontSize = 18;
             Instructions_Label.Content = "Welcome to ASTERIX APP!" + '\n' + '\n' + "We need some file to read!" + '\n' +
@@ -41,51 +41,30 @@ namespace ASTERIX_APP
         {
             OpenFileDialog OpenFile = new OpenFileDialog();
             OpenFile.ShowDialog();
-
-            Fichero fichero = new Fichero(OpenFile.FileName);
+            F = new Fichero(OpenFile.FileName);
             Instructions_Label.Content = "Loading...";
-            fichero.leer();
+
+            F.leer();
+            
             Instructions_Label.Content = "Perfectly read!" + '\n' + "1) View the displayed data by clicking on 'Tracking Table'" +
                     '\n' + "2) Run a data simulation by clicking on 'Tracking Map'";
-<<<<<<< HEAD
-                MapButton.Visibility = 0;
-                TableButton.Visibility = 0;
 
-                if (fichero.CAT == 10) { Track_Table.ItemsSource = fichero.getListCAT10(); }
-                if (fichero.CAT == 21) { Track_Table.ItemsSource = fichero.getListCAT21(); }
-          
-                else { MessageBox.Show("SLOW MOTION" + '\n' + "We have not solve this Asterix category yet"); }
-            }
-            catch
-            {
-                MessageBox.Show("There was an error. Incorrect format/Something went wrong in CLASSES");
-                // Hay un archivo q me da errores en el cat21
-            }
-        }
-=======
             MapButton.Visibility = Visibility.Visible; ;
             TableButton.Visibility = Visibility.Visible; ;
-        }    
->>>>>>> d54eb8397ec35802de314d53319a667c2e6283c1
+        }
         private void TableTrack_Click(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show(F.getListCAT10().Count().ToString());
+
             Instructions_Label.Visibility = Visibility.Hidden;
             Track_Table.Visibility = Visibility.Visible;
             map.Visibility = Visibility.Hidden;
         }
         private void MapTrack_Click(object sender, RoutedEventArgs e)
         {
-<<<<<<< HEAD
-            map.Visibility = 0;
-
-            List<CAT21> pepe = F.getListCAT21();
-
-
-=======
             Instructions_Label.Visibility = Visibility.Hidden;
             Track_Table.Visibility = Visibility.Hidden;
-            map.Visibility = Visibility.Visible; ;            
->>>>>>> d54eb8397ec35802de314d53319a667c2e6283c1
+            map.Visibility = Visibility.Visible;
         }
         private void Map_Load(object sender, RoutedEventArgs e)
         {
@@ -103,9 +82,7 @@ namespace ASTERIX_APP
             map.Zoom = 13;
             map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
             map.CanDragMap = true;
-            map.DragButton = MouseButton.Left;
-
-                
+            map.DragButton = MouseButton.Left;                
         }
     }
 }
