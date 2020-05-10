8659f37923a0680de30b983d2154430c38467a5f
santiagodelhierro98 <santidelhierro98@gmail.com>
2020-05-10 14:11:45 +0200

    cambio de variables de lat lon a cartesianas

--- CLASSES/Extrapoints.cs
+++ CLASSES/Extrapoints.cs
@@ -12,8 +12,6 @@ namespace CLASSES
     public class Extrapoints
     {
         //primer: filtrar per MLAT Cat 10 (Target_Rep_Descript[0] == "Mode S Multilateration")
-        List<CAT10> MLATList = new List<CAT10>();
-        List<CAT21> ADSBList = new List<CAT21>();
         Metodos M = new Metodos();
 
         public double ARP_lat = 41.0 + (17.0 / 60.0) + (49.0 / 3600.0) + (426.0 / 3600000.0);
@@ -22,22 +20,38 @@ namespace CLASSES
         public bool checkdistanceMLAT(CAT10 C10)
         {
             // el módulo del segmento que une la posición del ARP con la posición del avión debe ser inferior a 10 MN
-            double lat_MLAT = M.cartesiantolatmlat(C10.Pos_Cartesian[0], C10.Pos_Cartesian[1]);
-            double lon_MLAT = M.cartesiantolonmlat(C10.Pos_Cartesian[0], C10.Pos_Cartesian[1]);
-            double modulo = Math.Sqrt(Math.Pow((lat_MLAT - ARP_lat), 2) + Math.Pow((lon_MLAT - ARP_lon), 2));
-            if (modulo <= 10) { return  true; }
+            double pos_x = C10.Pos_Cartesian[0];
+            double pos_y = C10.Pos_Cartesian[0];
+            double modulo = Math.Sqrt(Math.Pow(pos_x, 2) + Math.Pow(pos_y, 2));
+            if (modulo <= 10 * 1852) { return  true; }
             else { return false; }
         }
-        public bool checkdistanceADSB(CAT21 C21)
+        public bool checkdistanceADSB(CAT21 C21,double x,double y)
         {
-            //límite de 10 MN (1 min 1 MN)
 
             // el módulo del segmento que une la posición del ARP con la posición del avión debe ser inferior a 10 MN
-            double modulo = Math.Sqrt(Math.Pow((C21.Lat_WGS_84 - ARP_lat), 2) + Math.Pow((C21.Lon_WGS_84 - ARP_lon), 2));
-            if (modulo <= 10) { return true; }
+            double modulo = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            if (modulo <= 10 * 1852) { return true; }
             else { return false; }
         }
-      
+        double x;
+        double y;
+        double z;
+        public void fromlatlontocartesian(double lat,double lon,double h)
+        {
+            //las coordenas latitud y longitud estan referenciadas al sistema elipsoidal WGS-84
+            //excentricidad
+            double a = 6378137.0; //in m
+            double b = 6356752.3142; //in m
+            double e = Math.Sqrt(1 - (Math.Pow(b, 2) / Math.Pow(a, 2)));
+            x = (a * Math.Cos(lat) / Math.Sqrt(1 + (1 - Math.Pow(e, 2) * Math.Pow(Math.Tan(lon), 2)))) + h * Math.Cos(lat) * Math.Cos(lon);
+            y = (a * Math.Sin(lat) / Math.Sqrt(1 + (1 - Math.Pow(e, 2) * Math.Pow(Math.Tan(lon), 2)))) + h * Math.Sin(lat) * Math.Cos(lon);
+            z = (a * (1 - Math.Pow(e, 2) * Math.Sin(lon) / Math.Sqrt(1 - Math.Pow(e, 2) * Math.Pow(Math.Sin(lon), 2)))) + h * Math.Sin(lon);
+        }
+        public double x_adsb()
+        { return x; }
+        public double y_adsb()
+        { return y; }
 
     }
 }
